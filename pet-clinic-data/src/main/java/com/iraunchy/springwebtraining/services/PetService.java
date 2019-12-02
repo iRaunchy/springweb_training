cf90f1bce339f924cd12a8da57f5dacabb1c4e6a
iRaunchy <iraunchy@gmail.com>
2019-12-02 23:36:07 +0100

    refactored interfaces. Closes #11

--- pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/PetService.java
+++ pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/PetService.java
@@ -2,14 +2,6 @@ package com.iraunchy.springwebtraining.services;
 
 import com.iraunchy.springwebtraining.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findByLastName(String lastName);
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/VetService.java
+++ pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/VetService.java
@@ -2,14 +2,7 @@ package com.iraunchy.springwebtraining.services;
 
 import com.iraunchy.springwebtraining.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
     Vet findByLastName(String lastName);
 
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
