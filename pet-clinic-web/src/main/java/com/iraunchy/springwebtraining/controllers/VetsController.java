d20cf2a2307baeea41d7b461a6afff013cfb74f3
iraunchy <iraunchy@gmail.com>
2020-02-02 19:15:58 +0100

    vets list

--- pet-clinic-web/src/main/java/com/iraunchy/springwebtraining/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/com/iraunchy/springwebtraining/controllers/VetsController.java
@@ -1,13 +1,22 @@
 package com.iraunchy.springwebtraining.controllers;
 
+import com.iraunchy.springwebtraining.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index.html", "vets/index"})
-    public String vetsList() {
+    public String vetsList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
