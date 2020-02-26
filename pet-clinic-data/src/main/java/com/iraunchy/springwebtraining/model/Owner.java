37afc1aa359ed1ed1aecad917005797064a2413c
iraunchy <iraunchy@gmail.com>
2020-02-26 22:51:38 +0100

    fixed Owner

--- pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/model/Owner.java
+++ pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/model/Owner.java
@@ -3,5 +3,14 @@ package com.iraunchy.springwebtraining.model;
 import java.util.Set;
 
 public class Owner extends Person {
-    private Set<Owner> ownersList;
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
