a23aead9bd7f1a9c758f7752a365f3530ed19467
iraunchy <iraunchy@gmail.com>
2020-02-27 21:52:42 +0100

    speciality bootstrap

--- pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.iraunchy.springwebtraining.services.map;
 
+import com.iraunchy.springwebtraining.model.Speciality;
 import com.iraunchy.springwebtraining.model.Vet;
+import com.iraunchy.springwebtraining.services.SpecialityService;
 import com.iraunchy.springwebtraining.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private SpecialityService specialityService;
+
+    public VetServiceMap(SpecialityService specialityService) {
+        this.specialityService = specialityService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -26,6 +34,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet vet) {
+        if (vet.getSpecialities().size() > 0) {
+            vet.getSpecialities().forEach(speciality -> {
+                if (speciality.getId() == null) {
+                    Speciality savedSpeciality = specialityService.save(speciality);
+                    speciality.setId(savedSpeciality.getId());
+                }
+            });
+        }
         return super.save(vet);
     }
 
--- pet-clinic-web/src/main/java/com/iraunchy/springwebtraining/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/iraunchy/springwebtraining/bootstrap/DataLoader.java
@@ -1,11 +1,9 @@
 package com.iraunchy.springwebtraining.bootstrap;
 
-import com.iraunchy.springwebtraining.model.Owner;
-import com.iraunchy.springwebtraining.model.Pet;
-import com.iraunchy.springwebtraining.model.PetType;
-import com.iraunchy.springwebtraining.model.Vet;
+import com.iraunchy.springwebtraining.model.*;
 import com.iraunchy.springwebtraining.services.OwnerService;
 import com.iraunchy.springwebtraining.services.PetTypeService;
+import com.iraunchy.springwebtraining.services.SpecialityService;
 import com.iraunchy.springwebtraining.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -18,15 +16,39 @@ public class DataLoader implements CommandLineRunner {
     private final OwnerService ownerService;
     private final VetService vetService;
     private final PetTypeService petTypeService;
+    private final SpecialityService specialityService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
+        this.specialityService = specialityService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        int count = petTypeService.findAll().size();
+        if (count == 0) {
+            loadData();
+        }
+
+    }
+
+    private void loadData() {
+        Speciality radiology = new Speciality();
+        radiology.setDescription("Radiology");
+        Speciality savedRadiology = specialityService.save(radiology);
+
+
+        Speciality surgery = new Speciality();
+        surgery.setDescription("Surgery");
+        Speciality savedSurgery = specialityService.save(surgery);
+
+        Speciality dentistry = new Speciality();
+        dentistry.setDescription("Dentistry");
+        Speciality savedDentistry = specialityService.save(dentistry);
+
 
         PetType dog = new PetType();
         dog.setName("Dog");
@@ -71,16 +93,17 @@ public class DataLoader implements CommandLineRunner {
         Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedSurgery);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedDentistry);
 
         vetService.save(vet2);
 
         System.out.println("Loaded vets....");
-
     }
 }
