0004af9bc69278dad79b9c4c11e70a10f7d81414
iraunchy <iraunchy@gmail.com>
2020-02-27 19:30:30 +0100

    broken code

--- pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/iraunchy/springwebtraining/services/map/OwnerServiceMap.java
@@ -2,12 +2,22 @@ package com.iraunchy.springwebtraining.services.map;
 
 import com.iraunchy.springwebtraining.model.Owner;
 import com.iraunchy.springwebtraining.services.OwnerService;
+import com.iraunchy.springwebtraining.services.PetService;
+import com.iraunchy.springwebtraining.services.PetTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
